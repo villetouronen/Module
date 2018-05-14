0ef4c943ed1ea92daf43e8facd53e7c3c0bb2c67
Ville Touronen <ville.touronen@myy.haaga-helia.fi>
2018-05-14 19:31:21 +0000

    Edited moduletest.sh script

--- moduletest.sh
+++ moduletest.sh
@@ -5,7 +5,6 @@
 setxkbmap fi
 sudo apt-get update
 sudo apt-get -y install git salt-minion
-sudo timedatectl set-timezone Europe/Helsinki
 
 echo 'master: localhost'|sudo tee /etc/salt/minion
 git clone https://github.com/villetouronen/Module
